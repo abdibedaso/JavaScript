91fb56a6f7f953d2967b6c375bacbf833627c7b2
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-02 16:12:37 -0500

    Imp window-object for DOM element objects

---
+++ 10-Document-Object-Model/window-object.js
@@ -0,0 +1,5 @@
+// The window object is the entire browser window; the top-level object in DOM hierarchy
+// -- Technically, all global code and variables become part of the window object
+
+// -- window.open pops up a new browser window
+window.open("http://foo.com/bar.html", "My Foo Window ", "width=900,height=600,scrollbars=1");
