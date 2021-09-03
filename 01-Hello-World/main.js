2fc28c2dce100049315555afbeae2997e1d0e37e
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-03 15:31:19 -0500

    create js files for hello world

---
+++ 01-Hello-World/main.js
@@ -0,0 +1,4 @@
+// the variable values can be accessed from other variable.js file
+console.log(firstName, lastName, country, state, city, age, isMarried);
+console.log(gravity, boilingPoint, PI); // 9.81, 100, 3.14
+console.log(fullName, job, live);
