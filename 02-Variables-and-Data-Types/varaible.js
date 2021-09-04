c373a19658b702e26c6e1a7412b88ef4a33964f3
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-04 01:45:59 -0500

    Create varaible.js

---
+++ 02-Variables-and-Data-Types/varaible.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'Abdi'; // First name of a person
+let lastName = 'Tufa'; // Last name of a person
+let country = 'USA'; // Country
+let state = 'Iowa'; // State
+let city = 'Fairfield'; // City
+let age = 24; // Age in years
+let isMarried = false;
+
+// Declaring variables with number values
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let fullName = 'Abdi',
+    job = 'FullStack Enginer',
+    live = 'USA';
