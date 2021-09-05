26b489bc1bd234ecce6ca85895bdb6631a6e8d14
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-05 16:12:28 -0500

    Imp screen-object for DOM element objects

---
+++ 10-Document-Object-Model/screen-object.js
@@ -0,0 +1,4 @@
+// The document object is information about the client's display screen
+
+var a = "Available Height: " + screen.availHeight; // Screen availHeight Property - Get the available height of your screen
+var b = "Total Width: " + screen.width; // Screen width Property - Get the total width of your screen
