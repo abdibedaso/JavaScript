7ac0a12a1f5f7fa5f2be75b020a9edeaacbbb83e
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-05 16:12:09 -0500

    Imp navigator-object for DOM element objects

---
+++ 10-Document-Object-Model/navigator-object.js
@@ -0,0 +1,3 @@
+// The navigator object is information about the web browser application
+
+var a = "Platform: " + navigator.platform; // Navigator platform Property - Get the version of your browser
