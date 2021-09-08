3f6f2389588c5a24807c9ffe7bbdcd4a3d62e9e0
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:55:05 -0500

    add string substr.js

---
+++ 02-Variables-and-Data-Types/string_methods/substr.js
@@ -0,0 +1,5 @@
+//substr(): It takes two arguments,the starting index and number of characters to slice.
+let string = 'JavaScript'
+console.log(string.substr(4, 6)) // Script
+let country = 'USA'
+console.log(country.substr(0, 2)) //US
