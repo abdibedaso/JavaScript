ff127f26918fef462f1c5e5a9f57cb1520853177
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:56:06 -0500

    add string trim.js

---
+++ 02-Variables-and-Data-Types/string_methods/trim.js
@@ -0,0 +1,7 @@
+//trim(): Removes trailing space in the beginning or the end of a string.
+let string = '   JavaScript Web Application Programming   '
+console.log(string) // 
+console.log(string.trim(' ')) // 
+let firstName = ' Abdi '
+console.log(firstName)
+console.log(firstName.trim()) //
