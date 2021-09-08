7022c2982f524b0a58c35af89315257c4f32f952
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:50:38 -0500

    create string_data_types.js

---
+++ 02-Variables-and-Data-Types/string_data_types.js
@@ -0,0 +1,7 @@
+let space = ' ' // an empty space string
+let firstName = 'Abdi'
+let lastName = 'Bedaso'
+let country = 'USA'
+let city = 'Fairfield'
+let language = 'JavaScript'
+let job = 'student'
