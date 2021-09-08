2d6fa83af5c9ad1374e0d17880fc07ae2c4b06f9
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:55:39 -0500

    add string to_lowercase.js

---
+++ 02-Variables-and-Data-Types/string_methods/to_lowercase.js
@@ -0,0 +1,7 @@
+// toLowerCase(): this method changes the string to lowercase letters.
+let string = 'JavasCript'
+console.log(string.toLowerCase()) // javascript
+let firstName = 'Abdi'
+console.log(firstName.toLowerCase()) // abdi
+let country = 'USA'
+console.log(country.toLowerCase()) // usa
