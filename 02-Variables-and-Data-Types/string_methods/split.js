df087a26a42ec127c23e7dd82bbffed0d07f949a
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:54:39 -0500

    add string split.js

---
+++ 02-Variables-and-Data-Types/string_methods/split.js
@@ -0,0 +1,10 @@
+// split(): The split method splits a string at a specified place.
+let string = 'JavaScript Web Application Programming'
+console.log(string.split()) // ["JavaScript Web Application Programming"]
+console.log(string.split(' ')) // ["JavaScript", "Web", "Application", "Programming"]
+let firstName = 'Abdi'
+console.log(firstName.split()) // ["Abdi"]
+console.log(firstName.split('')) // ["A", "b", "d", "i"]
+let countries = 'USA, Sweden, Norway, Denmark, and Iceland'
+console.log(countries.split(',')) // ["Finland", " Sweden", " Norway", " Denmark", " and Iceland"]
+console.log(countries.split(', ')) //  ["Finland", "Sweden", "Norway", "Denmark", "and Iceland"]
