1b13d0b43545ff08b77d89724221143643ce12dc
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:53:04 -0500

    add string index_of.js

---
+++ 02-Variables-and-Data-Types/string_methods/index_of.js
@@ -0,0 +1,7 @@
+// indexOf(): Takes takes a substring and if the substring exists in a string it returns the first position of the substring if does not exist it returns -1
+
+string.indexOf(substring)
+let string = 'JavaScript Web Application Programming'
+console.log(string.indexOf('a')) // 3
+console.log(string.indexOf('Web')) // 11
+console.log(string.indexOf('script')) // -1
