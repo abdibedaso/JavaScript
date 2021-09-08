181fb9b4f432057b1c2893ecd828522e376f81f0
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:55:57 -0500

    add string to_uppercase.js

---
+++ 02-Variables-and-Data-Types/string_methods/to_uppercase.js
@@ -0,0 +1,8 @@
+// toUpperCase(): this method changes the string to uppercase letters.
+
+let string = 'JavaScript'
+console.log(string.toUpperCase()) // JAVASCRIPT
+let firstName = 'Abdi'
+console.log(firstName.toUpperCase()) // ABDI
+let country = 'usa'
+console.log(country.toUpperCase()) // USA
