eb65b7b881c83b528b370bf5b5b5c4598b035f62
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:54:08 -0500

    add string replace.js

---
+++ 02-Variables-and-Data-Types/string_methods/replace.js
@@ -0,0 +1,7 @@
+// replace(): takes to parameter the old substring and new substring.
+// string.replace(oldsubstring, newsubstring)
+
+let string = 'JavaScript Web Application Programming'
+console.log(string.replace('JavaScript', 'Java'))
+let country = 'USA'
+console.log(country.replace('US', ''))
