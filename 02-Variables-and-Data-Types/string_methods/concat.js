275496b96914176f3cf1ec799b5c97afd0f0e941
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-07 21:52:17 -0500

    add string concat.js

---
+++ 02-Variables-and-Data-Types/string_methods/concat.js
@@ -0,0 +1,6 @@
+// concat(): it takes many substrings and creates concatenation.
+// string.concat(substring, substring, substring)
+let string = 'JavaScript'
+console.log(string.concat("Web", "Application", "Programming"))
+let country = 'US'
+console.log(country.concat("A")) // USA
