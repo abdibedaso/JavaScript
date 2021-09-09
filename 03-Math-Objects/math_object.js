2f8941896ca2d247ecb1a73ab66ad7789350b75a
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-09 12:14:48 -0500

    add Math.floor and Math.ceil to math-objects

--- 03-Math-Objects/math_object.js
+++ 03-Math-Objects/math_object.js
@@ -5,3 +5,6 @@ console.log(PI) // 3.141592653589793
 // Math.round
 console.log(Math.round(PI)) // 3; to round values to the nearest number
 console.log(Math.round(9.81)) // 10
+
+console.log(Math.floor(PI)) // 3; rounding down
+console.log(Math.ceil(PI)) // 4; rounding up
