247d6401bc7fe841111e83e0322d6164e12a0a68
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-09 12:15:38 -0500

    add Math.min and Math.max to math-objects

--- 03-Math-Objects/math_object.js
+++ 03-Math-Objects/math_object.js
@@ -8,3 +8,7 @@ console.log(Math.round(9.81)) // 10
 
 console.log(Math.floor(PI)) // 3; rounding down
 console.log(Math.ceil(PI)) // 4; rounding up
+
+// Math.min and Math.max
+console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5, returns the minimum value
+console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20, returns the maximum value
