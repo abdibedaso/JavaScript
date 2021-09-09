6140453b2500e4eef67bc9bfa13c9928e589d0e3
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-09 12:13:31 -0500

    add Math.round to math-objects

--- 03-Math-Objects/math_object.js
+++ 03-Math-Objects/math_object.js
@@ -1,2 +1,7 @@
+// Math.PI
 const PI = Math.PI
 console.log(PI) // 3.141592653589793
+
+// Math.round
+console.log(Math.round(PI)) // 3; to round values to the nearest number
+console.log(Math.round(9.81)) // 10
