e06a15f97b7a56508c93108248ecb10da7dd60d6
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-09 12:17:54 -0500

    add Math.pow to math-objects

--- 03-Math-Objects/math_object.js
+++ 03-Math-Objects/math_object.js
@@ -26,3 +26,7 @@ console.log(Math.abs(-10)) //10
 // Square root
 console.log(Math.sqrt(100)) // 10
 console.log(Math.sqrt(2)) //1.4142135623730951
+
+// Power
+console.log(Math.pow(3, 2)) // 9
+console.log(Math.E) // 2.718
