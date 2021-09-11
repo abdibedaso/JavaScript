7b12bcf9dea3df64b337a4700fc86f4f05b90338
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-11 16:11:54 -0500

    Imp location-object for DOM element objects

---
+++ 10-Document-Object-Model/location-object.js
@@ -0,0 +1,9 @@
+// The location object is the URL of the current web page
+
+var a = location.host; // Location host Property - Return the hostname and port of the current URL
+var b = location.hostname; // Location hostname Property - Return the hostname of the current URL
+var c = location.href; // Location href Property - Return the entire URL (of the current page)
+var d = "Port: " + location.port; // Location port Property - Return the port number of the current URL
+var e = location.pathname; // Location pathname Property - Return the path name of the current URL
+var f = location.protocol; //Location protocol Property - Return the protocol of the current URL
+var g = location.search; // Location search Property - Return the querystring part of a URL
