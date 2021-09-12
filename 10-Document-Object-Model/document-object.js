24b8cde629341a4e3594ca29a2c03c126987626b
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-12 16:10:27 -0500

    Imp document-object for DOM element objects

---
+++ 10-Document-Object-Model/document-object.js
@@ -0,0 +1,9 @@
+// The document object is the current web page and the elements inside it
+
+var a = document.anchors.length; // HTML DOM anchors Collection - Find out how many <a> elements there are in the document
+var b = document.cookie; // HTML DOM cookie Property - Get the cookies associated with the current document
+var c = document.domain; // HTML domain Property - Get the domain name of the server that loaded the document
+var d = document.forms.length; // HTML DOM forms Collection - Find out how many <form> elements there are in the document
+var e = document.images.length; // HTML DOM images Collection - Find out how many <img> elements there are in the document
+var f = document.images.title; // HTML DOM title Property - Get the title of the current document
+var g = document.images.length; // HTML DOM URL Property - Get the full URL of the current HTML document
