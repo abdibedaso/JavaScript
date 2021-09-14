6cceae36c8050e8e9e170b1fa673114d779a0981
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-14 08:23:42 -0500

    create array toString

---
+++ 05-Arrays/array-methods/array-toString.js
@@ -0,0 +1,6 @@
+// toString:Converts array to string
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
