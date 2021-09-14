14007fe362d08bdcd66a3fd21c3bf448066bbb03
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-14 08:17:52 -0500

    create if conditional

---
+++ 04-Conditionals/if.js
@@ -0,0 +1,15 @@
+// syntax
+if (condition) {
+    //this part of code runs for truthy condition
+}
+
+let num = 3
+if (num > 0) {
+    console.log(`${num} is a positive number`)
+}
+//  3 is a positive number
+
+let isRaining = true
+if (isRaining) {
+    console.log('Remember to take your rain coat.')
+}
