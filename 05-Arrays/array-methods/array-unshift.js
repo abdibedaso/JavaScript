ad8f18bf634548fa69c3f9f0b97c997d3ffc32fa
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-14 08:23:57 -0500

    create array unShift

---
+++ 05-Arrays/array-methods/array-unshift.js
@@ -0,0 +1,5 @@
+// unshift: Adding array element in the beginning of the array.
+const numbers = [1, 2, 3, 4, 5]
+numbers.unshift(0) // -> add one item from the beginning
+
+console.log(numbers) // -> [0,1,2,3,4,5]
