49c46d70f00384534ec92cb4d2f6a44f3c5ec88f
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-14 08:22:26 -0500

    create array joins

---
+++ 05-Arrays/array-methods/array-join.js
@@ -0,0 +1,26 @@
+// join: It is used to join the elements of the array, 
+// the argument we passed in the join method will be joined in the array and return as a string. 
+//By default, it joins with a comma, but we can pass different string parameter which can be joined between the items.
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.join()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+console.log(names.join()) // Asabeneh,Mathias,Elias,Brook
+console.log(names.join('')) //AsabenehMathiasEliasBrook
+console.log(names.join(' ')) //Asabeneh Mathias Elias Brook
+console.log(names.join(', ')) //Asabeneh, Mathias, Elias, Brook
+console.log(names.join(' # ')) //Asabeneh # Mathias # Elias # Brook
+
+const webTechs = [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React',
+        'Redux',
+        'Node',
+        'MongoDB'
+    ] // List of web technologies
+
+console.log(webTechs.join()) // "HTML,CSS,JavaScript,React,Redux,Node,MongoDB"
+console.log(webTechs.join(' # ')) // "HTML # CSS # JavaScript # React # Redux # Node # MongoDB"
