089723e3f403f3445cdf84dad724a971f2deefa9
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-14 08:21:45 -0500

    add array constructor

---
+++ 05-Arrays/array-methods/array-constructor.js
@@ -0,0 +1,6 @@
+// Array:To create an array.
+const arr = Array() // creates an an empty array
+console.log(arr)
+
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
