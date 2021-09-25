6a27b0d1499da704de690176cc69149d0bb6cc4d
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-24 22:00:37 -0500

    create Timer-Events setTimeout script

---
+++ 12-Timer-Events/setTimeout-script.js
@@ -0,0 +1,9 @@
+function delayMsg() {
+    setTimeout(booyah, 5000);
+    document.getElementById("output").innerHTML = "Wait for it...";
+}
+
+function booyah() {
+    // called when the timer goes off
+    document.getElementById("output").innerHTML = "BOOYAH!";
+}
