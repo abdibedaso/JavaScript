eb6440d1016d121721d9924c92cf40ff163071ab
Abdi T. Bedaso <86274569+abdibedaso@users.noreply.github.com>
2021-09-26 16:22:45 -0500

    Imp strict-mode

---
+++ 14-strict-mode/script.js
@@ -0,0 +1,9 @@
+// "use strict"; your code... at the very top of your JS file turns on strict syntax checking
+(function() {
+    "use strict";
+    alert("use strict");
+})();
+
+// Shows an error if you try to assign to an undeclared variable
+// Stops you from overwriting key JS system libraries
+// Forbids some unsafe or error-prone language features
